c4a997da38ce1396aa19f0a8b9fa3a2e1e813588
tudan110 <wtudan@126.com>
2024-08-02 15:36:53 +0800

    1.0.0

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,29 @@
-const LodashModuleReplacementPlugin = require("lodash-webpack-plugin")
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
 module.exports = {
-	publicPath: './',
-	productionSourceMap: false,
+    publicPath: './',
+    productionSourceMap: false,
     lintOnSave: true,
-	pages: {
-		index: {
-			entry: 'examples/main.js',
-			template: 'public/index.html',
-			filename: 'index.html'
-		}
-	},
-	configureWebpack:{
-    plugins: [
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/locale$/,
-        contextRegExp: /moment$/,
-      }),
-    ]
-  },
-  chainWebpack(config) {
-    if(process.env.analyzer) config.plugin('webpack-report').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    pages: {
+        index: {
+            entry: 'examples/main.js',
+            template: 'public/index.html',
+            filename: 'index.html'
+        }
+    },
+    configureWebpack: {
+        plugins: [
+            new webpack.IgnorePlugin({
+                resourceRegExp: /^\.\/locale$/,
+                contextRegExp: /moment$/,
+            }),
+        ]
+    },
+    chainWebpack(config) {
+        if (process.env.analyzer) config.plugin('webpack-report').use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
 
-    config.plugin("loadshReplace").use(new LodashModuleReplacementPlugin())
-  }
+        config.plugin('loadshReplace').use(new LodashModuleReplacementPlugin())
+    }
 
 }
